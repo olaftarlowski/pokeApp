bc615481f9d2a6fce3f7582f3dddb6324e05e47a
olaftarlowski <olaf.tarlowski@gmail.com>
2021-07-28 11:16:56 +0200

    FullList search option added, filter by name and id

--- src/components/PokemonFullList/PokemonFullList.js
+++ src/components/PokemonFullList/PokemonFullList.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { POKE_API } from "../../queries/Queries";
 import PokemonItem from "./Pokemon/PokemonItem";
 import styles from "./PokemonFullList.module.css";
@@ -7,22 +8,41 @@ const PokemonFullList = () => {
   const { loading, error, data } = useQuery(POKE_API, {
     variables: { limit: 26, offset: 0 },
   });
+
+  const [inputValue, setInputValue] = useState("");
+
+  const inputFilterHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   if (data) {
     return (
-      <div className={styles.wrapper}>
-        {data.pokemons.results.map((item) => {
-          return (
-            <PokemonItem
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-            />
-          );
-        })}
-      </div>
+      <>
+        <input type="text" value={inputValue} onChange={inputFilterHandler} placeholder="Enter name or number..."/>
+        <div className={styles.wrapper}>
+          {data.pokemons.results
+            .filter((filtItem) => {
+              if (!inputValue) return true;
+              const lowerCase = inputValue.toLowerCase()
+              return (
+                filtItem.id === +inputValue ||
+                filtItem.name.includes(lowerCase)
+              );
+            })
+            .map((item) => {
+              return (
+                <PokemonItem
+                  key={item.id}
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                />
+              );
+            })}
+        </div>
+      </>
     );
   }
 
@@ -34,3 +54,14 @@ const PokemonFullList = () => {
 };
 
 export default PokemonFullList;
+
+// {data.pokemons.results.map((item) => {
+//   return (
+//     <PokemonItem
+//       key={item.id}
+//       id={item.id}
+//       name={item.name}
+//       image={item.image}
+//     />
+//   );
+// })}
